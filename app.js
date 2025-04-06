efc1f5080e981fc11a12ae192fdf8bd065be1598
addaddudda <addaddudda>
2025-04-06 20:58:21 +0900

    유기 확정이다

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyparser = require('body-parser');
+let marked = require('marked');
 let fs = require('fs');
 let app = express();
 app.locals.pretty = true;
@@ -20,6 +21,7 @@ app.post('/memo', function(req, res){
     let description = req.body.description;
     fs.writeFile('data/' + title + '.md', description, function(err){
         if(err){
+            console.log(err);
             res.send('<h1>ERROR</h1>');
         }
         res.render('endmemo');
@@ -28,11 +30,34 @@ app.post('/memo', function(req, res){
 app.get('/check', function(req, res){
     fs.readdir('./data', function(err, files){
         if(err){
+            console.log(err);
             res.send('<h1>ERROR</h1>');
         }
         res.render('check', {files: files});
     })
 })
 app.get('/check/:id', function(req, res){
-    req
+    let id = req.params.id;
+    fs.readdir('data', (err, files) =>{
+        if(err){
+            console.log(err);
+            res.send('<h1>ERROR</h1>');
+        }
+        fs.readFile('data/' + id + '.md', 'utf8', (err, data)=>{
+            if(err){
+                console.log(err);
+                res.send('<h1>ERROR</h1>');
+            }
+            let markedhtml = marked(data);
+            res.send(`<html>
+                <head>
+                    <meta charset="utf-8">
+                    <title>${id}</title>
+                </head>
+                <body>
+                    ${markedhtml}
+                </body>
+                </html>`)
+        })
+    })
 })
